347575e112965c4bbaf51c889e16d9297817389a
Dummy-Bug <laxyrajput@gmail.com>
2024-08-03 11:59:37 +0530

    Adding changes

--- 06 Exploring The World/src/component/Body.js
+++ 06 Exploring The World/src/component/Body.js
@@ -1,38 +1,72 @@
 import RestaurantCard from "./RestaurantCard";
-import {restaurantList} from "../utils/mockData";
-import {useState} from "react";
+import { SWIGGY_API_URL } from "../utils/constant";
+import { useState, useEffect } from "react";
 
-//2. Body Component
+
+
 const Body = () => {
-    /*
-    Local state variable = Super powerful variable
-    restaurantList - default value
-    */
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
 
-    return <div className="body">
-        <button className="res-filter" onClick={() => 
-            {
-                const filteredList = restaurantList.filter((restaurants) => restaurants.data.avgRating > 4);
-                setListOfRestaurants(filteredList);//updating the state
-            }}>
-            Top Rated Restaurants
-        </button>
-        <button className="res-filter" onClick={() => 
-            {
-                const filteredList = restaurantList.filter((restaurants) => restaurants.data.avgRating < 4);
-                setListOfRestaurants(filteredList);//updating the state
-            }}>
-            Low Rated Restaurants
-        </button>
-        <div className="res-container">
-            {
-                listOfRestaurants.map((restaurant) => (
-                    <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
-                ))
-            }
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);//All Restaurants
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]); //Filtered Restaurants
+    const [searchRestaurant, setSearchRestaurant] = useState("");//Search Restaurants
+
+    // will call getRestraurants function once UI is rendered
+    useEffect(() => {
+        getRestaurants();
+    }, []);
+
+
+    const getRestaurants = async () => {
+
+        const data = await fetch(SWIGGY_API_URL);
+        const json = await data.json();
+        console.log("api data", json);
+        setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+
+    return (
+        <div className="body">
+
+            <div className="filter">
+
+                <input type="text" value={searchRestaurant}
+
+                    onChange={(e) => {
+
+                        setSearchRestaurant(e.target.value);
+                        
+                        const filteredRes = listOfRestaurants.filter((res) => {
+                            return res.info.name.toLowerCase().includes(searchRestaurant.toLowerCase())
+                        })
+
+                        setFilteredRestaurants(filteredRes);
+                    }}
+                />
+
+                <button className="res-filter" onClick={() => {
+                    const filteredList = listOfRestaurants.filter((restaurants) => restaurants.info.avgRating > 4);
+                    setFilteredRestaurants(filteredList);//updating the state
+                }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
+            <div className="res-container">
+                {
+                    (searchRestaurant.length > 0) ?
+                        //showing only filtered restaurants
+                        filteredRestaurants.map((restaurant) => (
+                            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                        ))
+                        :
+                        //showing all the restaurants
+                        listOfRestaurants.map((restaurant) => (
+                            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                        ))
+                }
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default Body;
